b8693da99c156d1c7e1b43a1c639023e95f122c6
Manu <manuela.vitorino@academico.ifpb.edu.br>
2025-04-22 11:33:48 -0300

    primeiros testes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,18 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
   <Stack>
-    <Stack.Screen name="index" options={{ title: "Teste"}} />
+    <Stack.Screen name="index"
+    options={{ title: "Home"}} 
+    />
     </Stack>
   );
+} 
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    jutifyContent: "center",
+    alignItens: "center"
+  }
 }
+)
--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text> Manu linda13 </Text>
     </View>
   );
 }
